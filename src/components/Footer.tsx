4995afb90ae8c31dcee13a9ba8e00910e1880c0c
Galabeewww <abirafdi722@gmail.com>
2026-08-15 18:20:36 +0700

    feat: modify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,24 +8,51 @@ const Footer = () => {
   return (
     <footer className="border-t border-[var(--border-color)] bg-[var(--bg-card)] py-8 mt-12 relative z-10 text-[var(--text-secondary)]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row items-center justify-between gap-4">
-        <p className="text-xs">
+        {/* <p className="text-xs">
           &copy; {currentYear} Muhammad Abi Rafdi Pratama. {t.footer.rights}
+        </p> */}
+        <p className="text-xs">
+          &copy; {currentYear}{" "}
+          <a
+            href="https://www.linkedin.com/in/muhammad-abi-rafdi-pratama-436044290/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-[var(--text-primary)] transition-colors hover:underline"
+          >
+            Muhammad Abi Rafdi Pratama
+          </a>
+          . {t.footer.rights}
         </p>
 
         <div className="flex items-center gap-6 text-xs font-medium">
-          <a href="#home" className="hover:text-[var(--text-primary)] transition-colors">
+          <a
+            href="#home"
+            className="hover:text-[var(--text-primary)] transition-colors"
+          >
             {t.nav.home}
           </a>
-          <a href="#about" className="hover:text-[var(--text-primary)] transition-colors">
+          <a
+            href="#about"
+            className="hover:text-[var(--text-primary)] transition-colors"
+          >
             {t.nav.about}
           </a>
-          <a href="#skills" className="hover:text-[var(--text-primary)] transition-colors">
+          <a
+            href="#skills"
+            className="hover:text-[var(--text-primary)] transition-colors"
+          >
             {t.nav.skills}
           </a>
-          <a href="#projects" className="hover:text-[var(--text-primary)] transition-colors">
+          <a
+            href="#projects"
+            className="hover:text-[var(--text-primary)] transition-colors"
+          >
             {t.nav.projects}
           </a>
-          <a href="#contact" className="hover:text-[var(--text-primary)] transition-colors">
+          <a
+            href="#contact"
+            className="hover:text-[var(--text-primary)] transition-colors"
+          >
             {t.nav.contact}
           </a>
         </div>
